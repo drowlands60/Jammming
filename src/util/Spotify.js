e9661f4db43bf98b3e753445645d03f0dbbe0c78
David Rowlands <drowlands60@gmail.com>
2020-10-27 19:49:00 +0000

    up to step 89

---
+++ src/util/Spotify.js
@@ -0,0 +1,51 @@
+import SearchBar from "../Components/SearchBar/SearchBar";
+
+let accessToken;
+const clientId = '1406c5171b6448219567ace77f607b55';
+const redirectURI = "http://localhost:3000/";
+
+let Spotify = {
+    getAccessToken(){
+        if (accessToken) {
+            return accessToken;
+        } 
+        const accessTokenMatch = window.locaction.href.match(/access_token=([^&]*)/);
+        const expiresInMatch = window.locaction.href.match(/expires_in=([^&]*)/);
+        if (accessTokenMatch && expiresInMatch){
+            accessToken = accessTokenMatch[1];
+            let expiresIn = Number(expiresInMatch[1]);
+            window.setTimeout(() => accessToken = '', expiresIn * 1000);
+            window.history.pushState('Access Token', null, '/');
+            return accessToken;
+        } else{
+            window.location = `https://accounts.spotify.com/authorize?client_id=${clientId}&response_type=token&scope=playlist-modify-public&redirect_uri=${redirectURI}`;
+        }
+    },
+
+    search(term){
+        const accessToken = Spotify.getAccessToken();
+        return fetch(`https://api.spotify.com/v1/search?type=track&q=${term}`, {
+            headers: {Authorization: `Bearer ${accessToken}`}
+            }).then(response => {
+              return response.json()
+            }).then( jsonResponse => {
+                if (!jsonResponse.tracks){
+                    return [];
+                }    
+                return jsonResponse.tracks.items.map(track =>({
+                        id: track.id,
+                        name: track.name,
+                        artist: track.artists[0].name,
+                        album: track.album.name,
+                        uri: track.uri
+
+                    })
+                );
+            }
+    
+        );
+        
+    }
+};
+
+export default Spotify;
